12b42073986163373bd5f4fbeea63860e5beb34a
Tom Gaffey <tom@b-21.org>
2020-08-27 03:30:31 +0000

    Removed omnibar dashboard menus and moved dashboards to Tools.

--- php-config/Slate/UI/Tools.config.d/administrators.php
+++ php-config/Slate/UI/Tools.config.d/administrators.php
@@ -5,3 +5,12 @@ if (empty($GLOBALS['Session']) || !$GLOBALS['Session']->Person || !$GLOBALS['Ses
 }
 //Duplicate the row below to add links to the tools menu that only administrators will see.
 //Slate\UI\Tools::$tools['<name>'] = '<URL>';
+Slate\UI\Tools::$tools = array_merge(Slate\UI\Tools::$tools, [
+    'School Compass' => [
+        'Advisory' => '/cbl/dashboards/admin/advisory',
+        'Attendance' => '/cbl/dashboards/admin/attendance',
+        'Teacher Survey' => '/cbl/dashboards/admin/teacher-survey',
+        'College and Career Readiness' => '/cbl/dashboards/admin/cc-readiness',
+        'Correlations' => '/cbl/dashboards/admin/correlations',
+    ]
+]);
